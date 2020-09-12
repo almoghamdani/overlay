403468374b34e46e43fc1eed6f9d7c3f141ff447
Almog Hamdani <almogh52@gmail.com>
2020-09-12 19:11:18 +0300

    refactor: change uint64_t in sprite to size_t

--- core/src/graphics/sprite.h
+++ core/src/graphics/sprite.h
@@ -9,8 +9,8 @@ namespace graphics {
 struct Sprite {
   std::vector<uint8_t> buffer;
 
-  uint64_t height, width;
-  uint64_t x, y;
+  size_t height, width;
+  size_t x, y;
 };
 
 }  // namespace graphics
