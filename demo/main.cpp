411c7762a17b8e40dc7c72f9fdd302fa87e1ed64
Almog Hamdani <almogh52@gmail.com>
2020-09-12 19:10:30 +0300

    feat: add options to the demo including connecting

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,39 +1,100 @@
 #include <Windows.h>
 #include <overlay/helper.h>
 
+#include <cxxopts.hpp>
 #include <iostream>
 #include <string>
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
-    std::cerr << "Usage: OverlayDemo.exe [Dest Program Path]" << std::endl;
-    return -1;
-  }
+  cxxopts::Options options("Overlay Demo",
+                           "This program is used to demonstrate the overlay");
 
-  std::string dest_program = argv[1];
+  options.add_options()("p,pid", "The pid of the process to inject the overlay",
+                        cxxopts::value<DWORD>())(
+      "e,executable",
+      "The name of the executable of the process to inject the overlay",
+      cxxopts::value<std::string>())("c,connect",
+                                     "Connect to the target process")(
+      "stats-log", "Log target process's stats (FPS and Frame time)")(
+      "h,help", "Show this help");
+
+  auto args = options.parse(argc, argv);
 
   STARTUPINFOA info = {sizeof(info)};
-  PROCESS_INFORMATION process_info;
+  PROCESS_INFORMATION process_info = {0};
 
-  // Try to create the dest process
-  std::cout << "Starting dest process.." << std::endl;
-  if (!CreateProcessA(dest_program.c_str(), "", NULL, NULL, TRUE, 0, NULL, NULL,
-                      &info, &process_info)) {
-    std::cerr << "Unable to start dest program!" << std::endl;
-    return -1;
+  ovhp::Client client;
+
+  // Show help
+  if (args.count("help") || !(args.count("pid") || args.count("executable"))) {
+    std::cout << options.help() << std::endl;
+    return 0;
   }
 
-  // Wait for process to load
-  Sleep(500);
+  if (args.count("executable")) {
+    // Try to create the dest process
+    std::cout << "Starting dest process.." << std::endl;
+    if (!CreateProcessA(args["executable"].as<std::string>().c_str(), "", NULL,
+                        NULL, TRUE, 0, NULL, NULL, &info, &process_info)) {
+      std::cerr << "Unable to start dest program!" << std::endl;
+      return -1;
+    }
+
+    // Wait for process to load
+    Sleep(300);
+  } else {
+    process_info.hProcess =
+        OpenProcess(SYNCHRONIZE, false, args["pid"].as<DWORD>());
+  }
 
   // Try to inject the overlay to the process
   std::cout << "Injecting to dest process.." << std::endl;
   try {
-    ovhp::InjectCoreToProcess(process_info.dwProcessId);
+    ovhp::InjectCoreToProcess(process_info.dwProcessId != 0
+                                  ? process_info.dwProcessId
+                                  : args["pid"].as<DWORD>());
   } catch (std::exception& ex) {
     std::cerr << ex.what() << std::endl;
     std::cerr << "Unable to inject overlay to process!" << std::endl;
     return -1;
+  }
+
+  // Wait for overlay to initiate
+#ifdef _WIN64
+  Sleep(1000);
+#else
+  Sleep(2500);
+#endif
+
+  if (args.count("connect")) {
+    try {
+      // Connect to the client
+      std::cout << "Connecting to overlay.." << std::endl;
+      client.ConnectToOverlay(process_info.dwProcessId != 0
+                                  ? process_info.dwProcessId
+                                  : args["pid"].as<DWORD>());
+      std::cout << "Connected to dest process' overlay!" << std::endl;
+
+      if (args["stats-log"].as<bool>()) {
+        client.SubscribeToEvent(
+            ovhp::EventType::ApplicationStats,
+            [](std::shared_ptr<ovhp::Event> event) {
+              printf(
+                  "Application stats: Frame time: %f, FPS: %f\n",
+                  std::static_pointer_cast<ovhp::ApplicationStatsEvent>(event)
+                      ->frame_time,
+                  std::static_pointer_cast<ovhp::ApplicationStatsEvent>(event)
+                      ->fps);
+            });
+      }
+
+      // Wait for process to exit
+      std::cout << "Waiting for process to exit.." << std::endl;
+      WaitForSingleObject(process_info.hProcess, INFINITE);
+    } catch (std::exception& ex) {
+      std::cerr << "An error occurred: " << ex.what() << std::endl;
+      return -1;
+    }
   }
 
   std::cout << "Done!" << std::endl;
